dd7d3f520ebaccbb28c317dd4f98efd10b68d6a5
WhitneyDluhosh <whitneydluhosh@gmail.com>
2020-06-04 14:20:21 -0700

    include the codic_V file reference

--- cordic_main.c
+++ cordic_main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include "cordic_V_fixed_point.c"
 int z_table[15] = { 25735, 15192, 8027, 4074, 2045, 1023,
   511, 255, 127, 63, 31, 15, 7, 3, 1};
 void cordic_V_fixed_point( int *x, int *y, int *z); /* defined elsewhere */
