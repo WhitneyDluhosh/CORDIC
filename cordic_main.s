3ed7baf96e775915f19120cdaf319b5b9d74fc95
root <root@localhost.localdomain>
2020-07-08 11:08:19 -0700

    compiled files and added assembly

--- cordic_main.s
+++ cordic_main.s
@@ -9,7 +9,7 @@
 	.eabi_attribute 30, 2
 	.eabi_attribute 34, 1
 	.eabi_attribute 18, 4
-	.file	"cordic_V_fixed_point.c"
+	.file	"cordic_main.c"
 	.text
 	.align	2
 	.global	cordic_V_fixed_point
@@ -36,10 +36,10 @@ cordic_V_fixed_point:
 	add	r9, r7, fp
 	ble	.L2
 	movw	r0, #:lower16:.LC1
-	movw	r8, #:lower16:z_table
+	movw	r8, #:lower16:.LANCHOR0
 	mov	r2, fp
 	movt	r0, #:upper16:.LC1
-	movt	r8, #:upper16:z_table
+	movt	r8, #:upper16:.LANCHOR0
 	mov	r1, #0
 	bl	printf
 	ldr	r10, [r8]
@@ -265,10 +265,10 @@ cordic_V_fixed_point:
 	pop	{r4, r5, r6, r7, r8, r9, r10, fp, pc}
 .L2:
 	movw	r0, #:lower16:.LC2
-	movw	r8, #:lower16:z_table
+	movw	r8, #:lower16:.LANCHOR0
 	mov	r2, fp
 	movt	r0, #:upper16:.LC2
-	movt	r8, #:upper16:z_table
+	movt	r8, #:upper16:.LANCHOR0
 	mov	r1, #0
 	bl	printf
 	ldr	r10, [r8]
@@ -432,7 +432,153 @@ cordic_V_fixed_point:
 	sub	r10, r10, r2
 	b	.L5
 	.size	cordic_V_fixed_point, .-cordic_V_fixed_point
-	.comm	z_table,60,4
+	.align	2
+	.global	verify
+	.syntax unified
+	.arm
+	.fpu vfpv3-d16
+	.type	verify, %function
+verify:
+	@ args = 8, pretend = 0, frame = 16
+	@ frame_needed = 0, uses_anonymous_args = 0
+	vmov	s15, r0	@ int
+	push	{r4, r5, lr}
+	vpush.64	{d8, d9, d10}
+	vldr.64	d9, .L38
+	vcvt.f64.s32	d10, s15
+	vmov	s15, r1	@ int
+	sub	sp, sp, #20
+	strd	r0, [sp]
+	vmul.f64	d10, d10, d9
+	movw	r0, #:lower16:.LC29
+	vcvt.f64.s32	d8, s15
+	strd	r2, [sp, #8]
+	ldr	r1, [sp]
+	vmov	r2, r3, d10
+	movt	r0, #:upper16:.LC29
+	vmul.f64	d8, d8, d9
+	ldr	r5, [sp, #56]
+	ldr	r4, [sp, #60]
+	bl	printf
+	movw	r0, #:lower16:.LC30
+	vmov	r2, r3, d8
+	ldr	r1, [sp, #4]
+	movt	r0, #:upper16:.LC30
+	bl	printf
+	vldr.32	s15, [sp, #8]	@ int
+	vcvt.f64.s32	d7, s15
+	movw	r0, #:lower16:.LC31
+	vmul.f64	d7, d7, d9
+	ldr	r1, [sp, #8]
+	vmov	r2, r3, d7
+	movt	r0, #:upper16:.LC31
+	bl	printf
+	vldr.32	s15, [sp, #12]	@ int
+	vcvt.f64.s32	d7, s15
+	movw	r0, #:lower16:.LC32
+	vmul.f64	d7, d7, d9
+	ldr	r1, [sp, #12]
+	vmov	r2, r3, d7
+	movt	r0, #:upper16:.LC32
+	bl	printf
+	vmov	s15, r5	@ int
+	vcvt.f64.s32	d7, s15
+	movw	r0, #:lower16:.LC33
+	vmul.f64	d7, d7, d9
+	mov	r1, r5
+	vmov	r2, r3, d7
+	movt	r0, #:upper16:.LC33
+	bl	printf
+	vmov	s15, r4	@ int
+	vcvt.f64.s32	d7, s15
+	movw	r0, #:lower16:.LC34
+	vmul.f64	d7, d7, d9
+	mov	r1, r4
+	vmov	r2, r3, d7
+	movt	r0, #:upper16:.LC34
+	bl	printf
+	vmul.f64	d8, d8, d8
+	vmla.f64	d8, d10, d10
+	vcmp.f64	d8, #0
+	vmrs	APSR_nzcv, FPSCR
+	vsqrt.f64	d9, d8
+	bmi	.L37
+.L35:
+	movw	r0, #:lower16:.LC35
+	vmov	r2, r3, d9
+	movt	r0, #:upper16:.LC35
+	add	sp, sp, #20
+	@ sp needed
+	vldm	sp!, {d8-d10}
+	pop	{r4, r5, lr}
+	b	printf
+.L37:
+	vmov.f64	d0, d8
+	bl	sqrt
+	b	.L35
+.L39:
+	.align	3
+.L38:
+	.word	0
+	.word	1056964608
+	.size	verify, .-verify
+	.section	.text.startup,"ax",%progbits
+	.align	2
+	.global	main
+	.syntax unified
+	.arm
+	.fpu vfpv3-d16
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 0, uses_anonymous_args = 0
+	movw	r0, #:lower16:.LC36
+	push	{r4, r5, lr}
+	movw	r4, #27852
+	movw	r5, #24903
+	sub	sp, sp, #28
+	movt	r0, #:upper16:.LC36
+	strd	r4, [sp, #12]
+	bl	puts
+	add	r2, sp, #20
+	add	r1, sp, #16
+	add	r0, sp, #12
+	bl	cordic_V_fixed_point
+	ldr	r2, [sp, #20]
+	ldr	r3, [sp, #16]
+	str	r2, [sp, #4]
+	str	r3, [sp]
+	mov	r1, r5
+	mov	r0, r4
+	ldr	r3, [sp, #12]
+	movw	r2, #23906
+	bl	verify
+	add	sp, sp, #28
+	@ sp needed
+	pop	{r4, r5, pc}
+	.size	main, .-main
+	.global	z_table
+	.data
+	.align	2
+	.set	.LANCHOR0,. + 0
+	.type	z_table, %object
+	.size	z_table, 60
+z_table:
+	.word	25735
+	.word	15192
+	.word	8027
+	.word	4074
+	.word	2045
+	.word	1023
+	.word	511
+	.word	255
+	.word	127
+	.word	63
+	.word	31
+	.word	15
+	.word	7
+	.word	3
+	.word	1
 	.section	.rodata.str1.4,"aMS",%progbits,1
 	.align	2
 .LC0:
@@ -508,5 +654,29 @@ cordic_V_fixed_point:
 	.ascii	"14: FIRST spot, y_temp_1 = %d \012\000"
 .LC28:
 	.ascii	"14: SECOND spot, y_temp_1 = %d \012\000"
+	.space	3
+.LC29:
+	.ascii	"x_i_init = %5i\011x_d_init = %f\012\000"
+	.space	2
+.LC30:
+	.ascii	"y_i_init = %5i\011y_d_init = %f\012\000"
+	.space	2
+.LC31:
+	.ascii	"z_i_init = %5i\011z_d_init = %f (rad)\012\012\000"
+	.space	3
+.LC32:
+	.ascii	"x_i_calc = %5i\011x_d_calc = %f\012\000"
+	.space	2
+.LC33:
+	.ascii	"y_i_calc = %5i\011y_d_calc = %f\012\000"
+	.space	2
+.LC34:
+	.ascii	"z_i_calc = %5i\011z_d_calc = %f (rad)\012\012\000"
+	.space	3
+.LC35:
+	.ascii	"Modulus = SQRT(x_d_init^2 + y_d_init^2) = %f\012\000"
+	.space	2
+.LC36:
+	.ascii	"Vectoring CORDIC:\012\000"
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
 	.section	.note.GNU-stack,"",%progbits
