fc56675e9c16fc2bb5422d01e87f86628ca5b9e7
Braydon Berthelet <braydonb90@gmail.com>
2020-07-08 09:55:51 -0700

    Software pipelining added to load the value from the table before it is needed allow greater parallelism

--- cordic_V_fixed_point.c
+++ cordic_V_fixed_point.c
@@ -1,71 +1,86 @@
 #include <stdio.h>
-int z_table[15];
+int z_t;
 
 void cordic_V_fixed_point( int *x, int *y, int *z) {
-register  int x_temp_1, y_temp_1, z_temp;
-register  int x_temp_2, y_temp_2;
-//  int i;
+  register  int x_temp_1, y_temp_1, z_temp, z_t;
+  register  int x_temp_2, y_temp_2;
+  
   x_temp_1 = *x;
   y_temp_1 = *y;
   z_temp = 0;
+  z_t = z_table[0];
   printf("ORIGINAL VALUE, y_temp_1 =  %d \n",y_temp_1);
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 0)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 0)), z_temp+= z_table[0]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 0)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 0)), z_temp -= z_table[0]);
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 0)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 0)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 0)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 0)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 1)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 1)), z_temp+= z_table[1]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 1)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 1)), z_temp -= z_table[1]);
+  z_t = z_table[1];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 1)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 1)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 1)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 1)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
   
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 2)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 2)), z_temp+= z_table[2]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 2)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 2)), z_temp -= z_table[2]);
+  z_t = z_table[2];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 2)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 2)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 2)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 2)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
   
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 3)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 3)), z_temp+= z_table[3]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 3)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 3)), z_temp -= z_table[3]);
+  z_t = z_table[3];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 3)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 3)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 3)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 3)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 4)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 4)), z_temp+= z_table[4]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 4)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 4)), z_temp -= z_table[4]);
+  z_t = z_table[4];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 4)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 4)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 4)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 4)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
- (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 5)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 5)), z_temp+= z_table[5]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 5)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 5)), z_temp -= z_table[5]);
+  z_t = z_table[5];
+ (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 5)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 5)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 5)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 5)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
   
- (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 6)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 6)), z_temp+= z_table[6]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 6)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 6)), z_temp -= z_table[6]);
+  z_t = z_table[6];
+ (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 6)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 6)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 6)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 6)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 7)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 7)), z_temp+= z_table[7]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 7)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 7)), z_temp -= z_table[7]);
+  z_t = z_table[7];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 7)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 7)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 7)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 7)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 8)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 8)), z_temp+= z_table[8]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 8)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 8)), z_temp -= z_table[8]);
+  z_t = z_table[8];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 8)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 8)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 8)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 8)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 9)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 9)), z_temp+= z_table[9]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 9)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 9)), z_temp -= z_table[9]);
+  z_t = z_table[9];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 9)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 9)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 9)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 9)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 10)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 10)), z_temp+= z_table[10]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 10)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 10)), z_temp -= z_table[10]);
+  z_t = z_table[10];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 10)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 10)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 10)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 10)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 11)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 11)), z_temp+= z_table[11]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 11)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 11)), z_temp -= z_table[11]);
+  z_t = z_table[11];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 11)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 11)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 11)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 11)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
   
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 12)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 12)), z_temp+= z_table[12]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 12)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 12)), z_temp -= z_table[12]);
+  z_t = z_table[12];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 12)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 12)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 12)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 12)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 13)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 13)), z_temp+= z_table[13]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 13)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 13)), z_temp -= z_table[13]);
+  z_t = z_table[13];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 13)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 13)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 13)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 13)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 14)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 14)), z_temp+= z_table[14]) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 14)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 14)), z_temp -= z_table[14]);
+  z_t = z_table[14];
+  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 14)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 14)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 14)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 14)), z_temp -= z_t);
   x_temp_1 = x_temp_2;
   y_temp_1 = y_temp_2;
 
