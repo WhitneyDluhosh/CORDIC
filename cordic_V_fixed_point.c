17d5c288dce9ceba6cd9ea1a63a45e8d91483259
Whitney Dluhosh <whitneydluhosh@gmail.com>
2020-07-15 13:07:50 -0700

    SIMD changes

--- cordic_V_fixed_point.c
+++ cordic_V_fixed_point.c
@@ -1,90 +1,76 @@
 #include <stdio.h>
+#include <arm_neon.h>
 int z_table[15];
-
+// add to compile instruction to get rid of soft error: -mfloat-abi=hard
 void cordic_V_fixed_point( int *x, int *y, int *z) {
-  register  int x_temp_1, y_temp_1, z_temp, z_t;
-  register  int x_temp_2, y_temp_2;
-  
-  x_temp_1 = *x;
-  y_temp_1 = *y;
+  register  int  z_temp, z_t;
+  int32x2_t XY;
+  int32x2_t shift_XY;
+  XY[0] = *x;
+  XY[1] = *y;
+  vhsl_s32(shift_XY, XY, 0);
   z_temp = 0;
   z_t = z_table[0];
-  printf("ORIGINAL VALUE, y_temp_1 =  %d \n",y_temp_1);
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 0)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 0)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 0)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 0)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 1);
 
   z_t = z_table[1];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 1)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 1)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 1)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 1)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
-  
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 2);
+
   z_t = z_table[2];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 2)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 2)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 2)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 2)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
-  
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 3);
+ 
   z_t = z_table[3];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 3)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 3)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 3)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 3)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 4);
 
   z_t = z_table[4];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 4)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 4)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 4)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 4)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 5);
 
   z_t = z_table[5];
- (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 5)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 5)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 5)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 5)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+ vhsl_s32(shift_XY, XY, 6);
   
   z_t = z_table[6];
- (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 6)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 6)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 6)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 6)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 7);
 
   z_t = z_table[7];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 7)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 7)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 7)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 7)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 8);
 
   z_t = z_table[8];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 8)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 8)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 8)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 8)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 9);
 
   z_t = z_table[9];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 9)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 9)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 9)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 9)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 10);
 
   z_t = z_table[10];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 10)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 10)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 10)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 10)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 11);
 
   z_t = z_table[11];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 11)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 11)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 11)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 11)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 12);
   
   z_t = z_table[12];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 12)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 12)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 12)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 12)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 13);
 
   z_t = z_table[13];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 13)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 13)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 13)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 13)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
+  vhsl_s32(shift_XY, XY, 14);
 
   z_t = z_table[14];
-  (y_temp_1 > 0 )? ( x_temp_2 = (x_temp_1 + (y_temp_1 >> 14)),y_temp_2 = (y_temp_1 - (x_temp_1 >> 14)), z_temp+= z_t) : (x_temp_2 = (x_temp_1 - (y_temp_1 >> 14)), y_temp_2 = (y_temp_1 + (x_temp_1 >> 14)), z_temp -= z_t);
-  x_temp_1 = x_temp_2;
-  y_temp_1 = y_temp_2;
+  (y_temp_1 > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
 
-  *x = x_temp_1;
-  *y = y_temp_1;
+
+  *x = XY[0];
+  *y = XY[1];
   *z = z_temp;
 }
--- cordic_V_fixed_point.s
+++ cordic_V_fixed_point.s
@@ -1,4 +1,4 @@
-	.arch armv7-a
+	.arch armv5t
 	.eabi_attribute 28, 1
 	.eabi_attribute 20, 1
 	.eabi_attribute 21, 1
@@ -7,188 +7,198 @@
 	.eabi_attribute 25, 1
 	.eabi_attribute 26, 2
 	.eabi_attribute 30, 2
-	.eabi_attribute 34, 1
+	.eabi_attribute 34, 0
 	.eabi_attribute 18, 4
 	.file	"cordic_V_fixed_point.c"
 	.text
 	.align	2
 	.global	cordic_V_fixed_point
-	.arch armv7-a
 	.syntax unified
 	.arm
-	.fpu vfpv3-d16
+	.fpu neon
 	.type	cordic_V_fixed_point, %function
 cordic_V_fixed_point:
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
-	push	{r4, r5, r6, r7, r8, r9, r10, lr}
-	mov	r6, r0
-	movw	r4, #:lower16:z_table
-	movw	r0, #:lower16:.LC0
-	ldr	r9, [r1]
-	ldr	r10, [r6]
-	movt	r4, #:upper16:z_table
-	mov	r5, r1
-	movt	r0, #:upper16:.LC0
-	mov	r1, r9
-	ldr	r8, [r4]
-	mov	r7, r2
-	bl	printf
-	add	r2, r10, r9
-	cmp	r9, #0
-	movle	r3, r2
-	subgt	r3, r9, r10
-	suble	r2, r10, r9
-	ldr	r1, [r4, #4]
-	asr	ip, r3, #1
-	asr	r0, r2, #1
-	rsble	r8, r8, #0
+	push	{r4, r5, r6, r7, r8, lr}
+	ldr	r7, [r1]
+	ldr	r8, [r0]
+	sub	sp, sp, #8
+	mov	r3, r7
+	mov	r5, r0
+	mov	r4, r1
+	mov	r6, r2
+	str	r7, [sp, #4]
+	mov	r2, r8
+	str	r8, [sp]
+	ldr	r1, .L34
+	mov	r0, #1
+	bl	__printf_chk
+	add	r1, r8, r7
+	cmp	r7, #0
+	movle	r3, r1
+	ldr	r2, .L34+4
+	subgt	r3, r7, r8
+	ldr	r0, [r2]
+	suble	r1, r8, r7
+	asr	lr, r1, #1
+	asr	r7, r3, #1
+	rsble	r0, r0, #0
+	ldr	ip, [r2, #4]
 	cmp	r3, #0
-	subgt	r3, r3, r0
-	addle	r3, r3, r0
-	addgt	r2, r2, ip
-	suble	r2, r2, ip
-	asr	r0, r2, #2
-	asr	ip, r3, #2
-	addgt	r1, r8, r1
-	suble	r1, r8, r1
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #2
+	asr	r7, r3, #2
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	ldr	r8, [r4, #8]
-	subgt	r3, r3, r0
-	addle	r3, r3, r0
-	addgt	r2, r2, ip
-	suble	r2, r2, ip
-	asr	r0, r2, #3
-	asr	ip, r3, #3
-	addgt	r1, r1, r8
-	suble	r1, r1, r8
+	ldr	ip, [r2, #8]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #3
+	asr	r7, r3, #3
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	ldr	r8, [r4, #12]
-	subgt	r3, r3, r0
-	addle	r3, r3, r0
-	addgt	r2, r2, ip
-	suble	r2, r2, ip
-	asr	r0, r2, #4
-	asr	ip, r3, #4
-	addgt	r1, r1, r8
-	suble	r1, r1, r8
+	ldr	ip, [r2, #12]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #4
+	asr	r7, r3, #4
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	ldr	r8, [r4, #16]
-	subgt	r3, r3, r0
-	addle	r3, r3, r0
-	addgt	r2, r2, ip
-	suble	r2, r2, ip
-	ldr	r0, [r4, #20]
-	asr	lr, r3, #5
-	asr	ip, r2, #5
-	addgt	r1, r1, r8
-	suble	r1, r1, r8
+	ldr	ip, [r2, #16]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #5
+	asr	r7, r3, #5
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	subgt	r3, r3, ip
-	addle	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #24]
+	ldr	ip, [r2, #20]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #6
+	asr	r7, r3, #6
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	asr	lr, r3, #6
-	asr	ip, r2, #6
-	subgt	r3, r3, ip
-	addle	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #28]
+	ldr	ip, [r2, #24]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #7
+	asr	r7, r3, #7
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	asr	lr, r3, #7
-	asr	ip, r2, #7
-	subgt	r3, r3, ip
-	addle	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #32]
+	ldr	ip, [r2, #28]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #8
+	asr	r7, r3, #8
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	asr	lr, r3, #8
-	asr	ip, r2, #8
-	subgt	r3, r3, ip
-	addle	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #36]
+	ldr	ip, [r2, #32]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #9
+	asr	r7, r3, #9
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	asr	lr, r3, #9
-	asr	ip, r2, #9
-	subgt	r3, r3, ip
-	addle	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #40]
+	ldr	ip, [r2, #36]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #10
+	asr	r7, r3, #10
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	asr	lr, r3, #10
-	asr	ip, r2, #10
-	subgt	r3, r3, ip
-	addle	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #44]
+	ldr	ip, [r2, #40]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #11
+	asr	r7, r3, #11
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	asr	lr, r3, #11
-	asr	ip, r2, #11
-	subgt	r3, r3, ip
-	addle	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #48]
+	ldr	ip, [r2, #44]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #12
+	asr	r7, r3, #12
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	asr	lr, r3, #12
-	asr	ip, r2, #12
-	subgt	r3, r3, ip
-	addle	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #52]
+	ldr	ip, [r2, #48]
+	subgt	r3, r3, lr
+	addle	r3, r3, lr
+	addgt	r1, r1, r7
+	suble	r1, r1, r7
+	asr	lr, r1, #13
+	asr	r7, r3, #13
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	cmp	r3, #0
-	asr	lr, r3, #13
-	asr	ip, r2, #13
-	subgt	r3, r3, ip
-	addgt	r2, r2, lr
-	suble	r2, r2, lr
-	addle	r3, r3, ip
-	addgt	r1, r1, r0
-	suble	r1, r1, r0
-	ldr	r0, [r4, #56]
-	cmp	r3, #0
+	ldr	ip, [r2, #52]
+	addgt	r1, r1, r7
+	ldr	r2, [r2, #56]
+	subgt	r3, r3, lr
+	suble	r1, r1, r7
+	addle	r3, r3, lr
+	addgt	r0, r0, ip
+	suble	r0, r0, ip
 	asr	lr, r3, #14
-	asr	ip, r2, #14
+	cmp	r3, #0
+	asr	ip, r1, #14
 	subgt	r3, r3, ip
-	addgt	r2, r2, lr
-	addgt	r1, r1, r0
-	suble	r2, r2, lr
+	addgt	r1, r1, lr
+	addgt	r0, r0, r2
+	suble	r1, r1, lr
 	addle	r3, r3, ip
-	suble	r1, r1, r0
-	str	r2, [r6]
-	str	r3, [r5]
-	str	r1, [r7]
-	pop	{r4, r5, r6, r7, r8, r9, r10, pc}
+	suble	r0, r0, r2
+	str	r1, [r5]
+	str	r3, [r4]
+	str	r0, [r6]
+	add	sp, sp, #8
+	@ sp needed
+	pop	{r4, r5, r6, r7, r8, pc}
+.L35:
+	.align	2
+.L34:
+	.word	.LC0
+	.word	z_table
 	.size	cordic_V_fixed_point, .-cordic_V_fixed_point
 	.comm	z_table,60,4
 	.section	.rodata.str1.4,"aMS",%progbits,1
 	.align	2
 .LC0:
-	.ascii	"ORIGINAL VALUE, y_temp_1 =  %d \012\000"
-	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
+	.ascii	"new XY: %d  and shifted XY: %d original X: %d origi"
+	.ascii	"nal Y: %d\000"
+	.ident	"GCC: (Ubuntu/Linaro 7.5.0-3ubuntu1~18.04) 7.5.0"
 	.section	.note.GNU-stack,"",%progbits
