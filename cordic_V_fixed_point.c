006a439e332323c6f63cbf024bbdd6212ae00724
Whitney Dluhosh <whitneydluhosh@gmail.com>
2020-07-17 16:05:38 -0700

    current SIMD files

--- cordic_V_fixed_point.c
+++ cordic_V_fixed_point.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <arm_neon.h>
+#include <arm_acle.h>
 int z_table[15];
 // add to compile instruction to get rid of soft error: -mfloat-abi=hard
 void cordic_V_fixed_point( int *x, int *y, int *z) {
@@ -8,63 +9,63 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   int32x2_t shift_XY;
   XY[0] = *x;
   XY[1] = *y;
-  vhsl_s32(shift_XY, XY, 0);
+  vshl_s32(shift_XY, XY, 0);
   z_temp = 0;
   z_t = z_table[0];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 1);
+  vshl_s32(shift_XY, XY, 1);
 
   z_t = z_table[1];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 2);
+  vshl_s32(shift_XY, XY, 2);
 
   z_t = z_table[2];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 3);
+  vshl_s32(shift_XY, XY, 3);
  
   z_t = z_table[3];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 4);
+  vshl_s32(shift_XY, XY, 4);
 
   z_t = z_table[4];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 5);
+  vshl_s32(shift_XY, XY, 5);
 
   z_t = z_table[5];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
- vhsl_s32(shift_XY, XY, 6);
+ vshl_s32(shift_XY, XY, 6);
   
   z_t = z_table[6];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 7);
+  vshl_s32(shift_XY, XY, 7);
 
   z_t = z_table[7];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 8);
+  vshl_s32(shift_XY, XY, 8);
 
   z_t = z_table[8];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 9);
+  vshl_s32(shift_XY, XY, 9);
 
   z_t = z_table[9];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 10);
+  vshl_s32(shift_XY, XY, 10);
 
   z_t = z_table[10];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 11);
+  vshl_s32(shift_XY, XY, 11);
 
   z_t = z_table[11];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 12);
+  vshl_s32(shift_XY, XY, 12);
   
   z_t = z_table[12];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 13);
+  vshl_s32(shift_XY, XY, 13);
 
   z_t = z_table[13];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
-  vhsl_s32(shift_XY, XY, 14);
+  vshl_s32(shift_XY, XY, 14);
 
   z_t = z_table[14];
   (XY[1] > 0 )? (XY = __ssax(XY, shift_XY), z_temp+= z_t) : (XY = __sasx_(XY, shift_XY) , z_temp -= z_t);
