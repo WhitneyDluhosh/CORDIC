d6d7027728e75971d5424cd5a7c89b7aa3222e92
Whitney Dluhosh <whitneydluhosh@gmail.com>
2020-07-20 17:33:10 -0700

    Final SIMD iteration

--- cordic_V_fixed_point.c
+++ cordic_V_fixed_point.c
@@ -15,113 +15,77 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   int32x2_t temp;
   z_temp = 0;
   z_t = z_table[0];
-  __asm__ __volatile__ ( 
-    "sasx %0, %1, %2"
-    : "=r" (temp)
-    : "r" (XY[0]), "r" (shift_XY[1])
-   );
-  XY[0] = temp[0] >> 16;
-  XY[1] = temp[0] & 0x0000FFFF;
 
-  printf("0:  FIRST spot, X; %d Y: %d \n", XY[0], XY[1] );
-  XY[0] = *x;
-  XY[1] = *y;
   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 1);
 
   z_t = z_table[1];
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 2);
   
 
   z_t = z_table[2];
-  printf("2:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 3);
   
   
   z_t = z_table[3];
-  printf("3:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 4);
   
 
   z_t = z_table[4];
-  printf("4:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 5);
   
 
   z_t = z_table[5];
-  printf("5:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 6);
   
   
   z_t = z_table[6];
-  printf("6:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 7);
   
 
   z_t = z_table[7];
-  printf("7:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 8);
   
 
   z_t = z_table[8];
-  printf("8:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 9);
   
 
   z_t = z_table[9];
-  printf("9:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 10);
   
 
   z_t = z_table[10];
-  printf("10:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 11);
   
 
   z_t = z_table[11];
-  printf("11:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 12);
   
   
   z_t = z_table[12];
-  printf("12:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 13);
   
 
   z_t = z_table[13];
-  printf("13:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
-
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
   shift_XY = vshr_n_s32(XY, 14);
   
 
   z_t = z_table[14];
-  printf("14:  FIRST spot, y_temp_1 = %d\n", XY[1] );
-   (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
+  (XY[1] > 0 )? (XY[0] = XY[0] + shift_XY[1], XY[1] = XY[1] - shift_XY[0] , z_temp+= z_t) : (XY[0] = XY[0] - shift_XY[1], XY[1] = XY[1] + shift_XY[0] , z_temp -= z_t);
 
 
 
--- cordic_V_fixed_point.s
+++ cordic_V_fixed_point.s
@@ -20,503 +20,371 @@
 cordic_V_fixed_point:
 	@ args = 0, pretend = 0, frame = 0
 	@ frame_needed = 0, uses_anonymous_args = 0
-	push	{r4, r5, r6, r7, r8, r9, r10, lr}
-	movw	r5, #:lower16:z_table
-	vpush.64	{d8, d9}
-	movt	r5, #:upper16:z_table
-	ldr	r9, [r1]
-	mov	r7, r0
-	mov	r6, r1
-	mov	r8, r2
-	vld1.32	{d8[]}, [r0]
-	ldr	r4, [r5]
-	vmov.32	d8[1], r9
-	vmov.32	r10, d8[0]
-	.syntax divided
-@ 18 "cordic_V_fixed_point.c" 1
-	sasx_V r2, r10, r9
-@ 0 "" 2
-	.arm
-	.syntax unified
-	vmov	d16, r2, r3  @ v2si
-	movw	r1, #:lower16:.LC0
-	mov	r0, #1
-	movt	r1, #:upper16:.LC0
-	vmov.32	r2, d16[0]
-	asr	r3, r2, #16
-	uxth	r2, r2
-	vmov.32	d8[0], r3
-	mov	r3, r2
-	vmov.32	d8[1], r2
-	vmov.32	r2, d8[0]
-	bl	__printf_chk
-	ldr	r3, [r6]
-	vld1.32	{d8[0]}, [r7]
-	cmp	r3, #0
-	vmov.32	d8[1], r3
-	vmov.32	r3, d8[0]
+	ldr	ip, [r1]
+	movw	r3, #:lower16:z_table
+	vld1.32	{d16[]}, [r0]
+	movt	r3, #:upper16:z_table
+	cmp	ip, #0
+	push	{r4, r5, r6, lr}
+	vmov.32	d16[1], ip
+	ldr	r4, [r3]
+	vmov.32	lr, d16[0]
 	ble	.L2
-	add	r9, r9, r3
-	vmov.32	d8[0], r9
-	vmov.32	r3, d8[1]
-	sub	r10, r3, r10
-	vmov.32	d8[1], r10
+	add	ip, ip, lr
+	vmov.32	d16[0], ip
+	vmov.32	ip, d16[1]
+	sub	lr, ip, lr
+	vmov.32	d16[1], lr
 .L3:
-	vmov.32	r3, d8[1]
-	vshr.s32	d16, d8, #1
-	ldr	r0, [r5, #4]
-	vmov.32	r2, d8[0]
-	vmov.32	r1, d16[1]
-	cmp	r3, #0
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #1
+	ldr	r6, [r3, #4]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L4
-	add	r2, r1, r2
-	vmov.32	r3, d16[0]
-	add	r4, r4, r0
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L5:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC1
-	mov	r0, #1
-	movt	r1, #:upper16:.LC1
-	vshr.s32	d9, d8, #2
-	ldr	r10, [r5, #8]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #2
+	ldr	r6, [r3, #8]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L6
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L7:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC2
-	mov	r0, #1
-	movt	r1, #:upper16:.LC2
-	vshr.s32	d9, d8, #3
-	ldr	r10, [r5, #12]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #3
+	ldr	r6, [r3, #12]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L8
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L9:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC3
-	mov	r0, #1
-	movt	r1, #:upper16:.LC3
-	vshr.s32	d9, d8, #4
-	ldr	r10, [r5, #16]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #4
+	ldr	r6, [r3, #16]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L10
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L11:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC4
-	mov	r0, #1
-	movt	r1, #:upper16:.LC4
-	vshr.s32	d9, d8, #5
-	ldr	r10, [r5, #20]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #5
+	ldr	r6, [r3, #20]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L12
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L13:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC5
-	mov	r0, #1
-	movt	r1, #:upper16:.LC5
-	vshr.s32	d9, d8, #6
-	ldr	r10, [r5, #24]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #6
+	ldr	r6, [r3, #24]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L14
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L15:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC6
-	mov	r0, #1
-	movt	r1, #:upper16:.LC6
-	vshr.s32	d9, d8, #7
-	ldr	r10, [r5, #28]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #7
+	ldr	r6, [r3, #28]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L16
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L17:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC7
-	mov	r0, #1
-	movt	r1, #:upper16:.LC7
-	vshr.s32	d9, d8, #8
-	ldr	r10, [r5, #32]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #8
+	ldr	r6, [r3, #32]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L18
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L19:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC8
-	mov	r0, #1
-	movt	r1, #:upper16:.LC8
-	vshr.s32	d9, d8, #9
-	ldr	r10, [r5, #36]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #9
+	ldr	r6, [r3, #36]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L20
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L21:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC9
-	mov	r0, #1
-	movt	r1, #:upper16:.LC9
-	vshr.s32	d9, d8, #10
-	ldr	r10, [r5, #40]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #10
+	ldr	r6, [r3, #40]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L22
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L23:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC10
-	mov	r0, #1
-	movt	r1, #:upper16:.LC10
-	vshr.s32	d9, d8, #11
-	ldr	r10, [r5, #44]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #11
+	ldr	r6, [r3, #44]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L24
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L25:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC11
-	mov	r0, #1
-	movt	r1, #:upper16:.LC11
-	vshr.s32	d9, d8, #12
-	ldr	r10, [r5, #48]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #12
+	ldr	r6, [r3, #48]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L26
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L27:
-	vmov.32	r9, d8[1]
-	movw	r1, #:lower16:.LC12
-	mov	r0, #1
-	movt	r1, #:upper16:.LC12
-	vshr.s32	d9, d8, #13
-	ldr	r10, [r5, #52]
-	mov	r2, r9
-	bl	__printf_chk
-	cmp	r9, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	vmov.32	ip, d16[1]
+	vshr.s32	d17, d16, #13
+	ldr	r6, [r3, #52]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	ip, #0
 	ble	.L28
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	ip, d17[0]
+	add	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	ip, lr, ip
+	vmov.32	d16[1], ip
 .L29:
-	vmov.32	r3, d8[1]
-	movw	r1, #:lower16:.LC13
-	mov	r0, #1
-	movt	r1, #:upper16:.LC13
-	vshr.s32	d9, d8, #14
-	ldr	r9, [r5, #56]
-	mov	r2, r3
-	mov	r5, r3
-	bl	__printf_chk
-	cmp	r5, #0
-	vmov.32	r1, d9[1]
-	vmov.32	r2, d8[0]
+	ldr	ip, [r3, #56]
+	vshr.s32	d17, d16, #14
+	vmov.32	r3, d16[1]
+	vmov.32	lr, d16[0]
+	vmov.32	r5, d17[1]
+	cmp	r3, #0
 	ble	.L30
-	add	r2, r1, r2
-	vmov.32	r3, d9[0]
-	add	r4, r4, r9
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	sub	r3, r2, r3
-	vmov.32	d8[1], r3
+	add	lr, r5, lr
+	vmov.32	r3, d17[0]
+	add	ip, r4, ip
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	sub	r3, lr, r3
+	vmov.32	d16[1], r3
 .L31:
-	vst1.32	{d8[0]}, [r7]
-	vst1.32	{d8[1]}, [r6]
-	vldm	sp!, {d8-d9}
-	str	r4, [r8]
-	pop	{r4, r5, r6, r7, r8, r9, r10, pc}
+	vst1.32	{d16[0]}, [r0]
+	vst1.32	{d16[1]}, [r1]
+	str	ip, [r2]
+	pop	{r4, r5, r6, pc}
 .L30:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r9
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	r3, d17[0]
+	sub	ip, r4, ip
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	r3, lr, r3
+	vmov.32	d16[1], r3
 	b	.L31
 .L28:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L29
 .L26:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L27
 .L24:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L25
 .L22:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L23
 .L20:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L21
 .L18:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L19
 .L16:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L17
 .L14:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L15
 .L12:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L13
 .L10:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L11
 .L8:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L9
 .L6:
-	sub	r2, r2, r1
-	vmov.32	r3, d9[0]
-	sub	r4, r4, r10
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L7
 .L4:
-	sub	r2, r2, r1
-	vmov.32	r3, d16[0]
-	sub	r4, r4, r0
-	vmov.32	d8[0], r2
-	vmov.32	r2, d8[1]
-	add	r3, r2, r3
-	vmov.32	d8[1], r3
+	sub	lr, lr, r5
+	vmov.32	ip, d17[0]
+	sub	r4, r4, r6
+	vmov.32	d16[0], lr
+	vmov.32	lr, d16[1]
+	add	ip, lr, ip
+	vmov.32	d16[1], ip
 	b	.L5
 .L2:
-	sub	r9, r3, r9
+	sub	ip, lr, ip
 	rsb	r4, r4, #0
-	vmov.32	d8[0], r9
-	vmov.32	r3, d8[1]
-	add	r10, r3, r10
-	vmov.32	d8[1], r10
+	vmov.32	d16[0], ip
+	vmov.32	ip, d16[1]
+	add	lr, ip, lr
+	vmov.32	d16[1], lr
 	b	.L3
 	.size	cordic_V_fixed_point, .-cordic_V_fixed_point
 	.comm	z_table,60,4
-	.section	.rodata.str1.4,"aMS",%progbits,1
-	.align	2
-.LC0:
-	.ascii	"0:  FIRST spot, X; %d Y: %d \012\000"
-	.space	2
-.LC1:
-	.ascii	"2:  FIRST spot, y_temp_1 = %d\012\000"
-	.space	1
-.LC2:
-	.ascii	"3:  FIRST spot, y_temp_1 = %d\012\000"
-	.space	1
-.LC3:
-	.ascii	"4:  FIRST spot, y_temp_1 = %d\012\000"
-	.space	1
-.LC4:
-	.ascii	"5:  FIRST spot, y_temp_1 = %d\012\000"
-	.space	1
-.LC5:
-	.ascii	"6:  FIRST spot, y_temp_1 = %d\012\000"
-	.space	1
-.LC6:
-	.ascii	"7:  FIRST spot, y_temp_1 = %d\012\000"
-	.space	1
-.LC7:
-	.ascii	"8:  FIRST spot, y_temp_1 = %d\012\000"
-	.space	1
-.LC8:
-	.ascii	"9:  FIRST spot, y_temp_1 = %d\012\000"
-	.space	1
-.LC9:
-	.ascii	"10:  FIRST spot, y_temp_1 = %d\012\000"
-.LC10:
-	.ascii	"11:  FIRST spot, y_temp_1 = %d\012\000"
-.LC11:
-	.ascii	"12:  FIRST spot, y_temp_1 = %d\012\000"
-.LC12:
-	.ascii	"13:  FIRST spot, y_temp_1 = %d\012\000"
-.LC13:
-	.ascii	"14:  FIRST spot, y_temp_1 = %d\012\000"
 	.ident	"GCC: (Ubuntu/Linaro 7.5.0-3ubuntu1~18.04) 7.5.0"
 	.section	.note.GNU-stack,"",%progbits
