31b55a101d6eb2dc8897b2faa2bddf956f8997bf
Braydon Berthelet <braydonb90@gmail.com>
2020-08-06 13:14:11 -0700

    Test

--- cordic_V_fixed_point.c
+++ cordic_V_fixed_point.c
@@ -1,94 +1,45 @@
 #include <arm_neon.h>
 #include <stdio.h>
 
+#define OP(XY, shift_XY, shift, vt, vf, cmp, ADDSUB, SUBADD) {\
+  shift_XY = vshr_n_s32(XY, shift);\
+  shift_XY = vrev64_s32(shift_XY);\
+  vt = veor_s32(shift_XY, ADDSUB);\
+  vf = veor_s32(shift_XY, SUBADD);\
+  cmp = vcgt_s32(XY, ZERO);\
+  cmp = vdup_lane_u32(cmp, 1);\ // Only use Y's value
+  XY = vadd_s32(XY, vbsl_s32(cmp, vt, vf));\
+}
+
 int z_table[15];
 // add to compile instruction to get rid of soft error: -mfloat-abi=hard and -mfpu=neon
 void cordic_V_fixed_point( int *x, int *y, int *z) {
   register  int  z_temp, z_t;
-  int32x2_t XY;
+  int32x2_t vt, vf, cmp; // Value true, Value false, Compare mask
+  int32x2_t XY = {*x, *y};
   int32x2_t shift_XY;
-  int32x2_t ADDSUB = {1, -1};
-  int32x2_t SUBADD = {-1, 1};
-  XY[0] = *x;
-  XY[1] = *y;
-  shift_XY = XY;
+  int32x2_t ZERO = {0,0};
+  // int32x2_t ADDSUB = {1, -1};
+  // int32x2_t SUBADD = {-1, 1};
+  const int32x2_t ADDSUB = {0x0, 0xFFFFFFFE};
+  const int32x2_t SUBADD = {0xFFFFFFFE, 0x0};
+  
   z_temp = 0;
-  z_t = z_table[0];
-  shift_XY = vrev64_s32(XY);
-  (XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  printf("X: %d Y: %d \n",XY[0],XY[1]);
-  shift_XY = vshr_n_s32(XY, 1);
-  shift_XY = vrev64_s32(XY);
-  
-  z_t = z_table[1];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 2);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[2];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 3);
-  shift_XY = vrev64_s32(XY);
-  
-  z_t = z_table[3];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 4);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[4];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 5);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[5];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 6);
-  shift_XY = vrev64_s32(XY);
-  
-  z_t = z_table[6];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 7);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[7];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 8);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[8];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 9);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[9];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 10);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[10];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 11);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[11];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 12);
-  shift_XY = vrev64_s32(XY);
-  
-  z_t = z_table[12];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 13);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[13];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-  shift_XY = vshr_n_s32(XY, 14);
-  shift_XY = vrev64_s32(XY);
-
-  z_t = z_table[14];
-(XY[1] > 0 )? (XY = vadd_s32(XY, vmul_s32(shift_XY,ADDSUB)), z_temp+= z_t) : (XY = vadd_s32(XY, vmul_s32(shift_XY,SUBADD)), z_temp -= z_t);
-
-
+  OP(XY, shift_XY, 0, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 1, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 2, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 3, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 4, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 5, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 6, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 7, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 8, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 9, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 10, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 11, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 12, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 13, vt, vf, cmp, ADDSUB, SUBADD);
+  OP(XY, shift_XY, 14, vt, vf, cmp, ADDSUB, SUBADD);
 
   *x = XY[0];
   *y = XY[1];
