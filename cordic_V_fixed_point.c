aac62597a10cbf12182c5bf58803817ada6c213e
WhitneyDluhosh <whitneydluhosh@gmail.com>
2020-08-06 10:25:18 -0700

    update to remove printf statements

--- cordic_V_fixed_point.c
+++ cordic_V_fixed_point.c
@@ -8,15 +8,12 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   x_temp_1 = *x;
   y_temp_1 = *y;
   z_temp = 0;
-  printf("ORIGINAL VALUE, y_temp_1 =  %d \n",y_temp_1);
   if( y_temp_1 > 0) {
-    printf("%d: FIRST spot, y_temp_1 = %d \n",0,y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 0);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 0);
     z_temp += z_table[0];
   }
   else {
-    printf("%d: SECOND spot, y_temp_1 = %d \n",0, y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 0);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 0);
     z_temp -= z_table[0];
@@ -25,13 +22,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("%d: FIRST spot, y_temp_1 = %d \n",1,y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 1);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 1);
     z_temp += z_table[1];
   }
   else {
-    printf("%d: SECOND spot, y_temp_1 = %d \n",1, y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 1);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 1);
     z_temp -= z_table[1];
@@ -40,13 +35,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("2: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 2);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 2);
     z_temp += z_table[2];
   }
   else {
-    printf("2: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 2);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 2);
     z_temp -= z_table[2];
@@ -55,13 +48,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("3: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 3);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 3);
     z_temp += z_table[3];
   }
   else {
-    printf("3 SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 3);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 3);
     z_temp -= z_table[3];
@@ -70,13 +61,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("4: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 4);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 4);
     z_temp += z_table[4];
   }
   else {
-    printf("4: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 4);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 4);
     z_temp -= z_table[4];
@@ -85,13 +74,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("5: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 5);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 5);
     z_temp += z_table[5];
   }
   else {
-    printf("5: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 5);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 5);
     z_temp -= z_table[5];
@@ -100,13 +87,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
   
   if( y_temp_1 > 0) {
-    printf("6: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 6);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 6);
     z_temp += z_table[6];
   }
   else {
-    printf("6: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 6);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 6);
     z_temp -= z_table[6];
@@ -115,13 +100,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("7: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 7);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 7);
     z_temp += z_table[7];
   }
   else {
-    printf("7: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 7);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 7);
     z_temp -= z_table[7];
@@ -130,13 +113,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("8: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 8);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 8);
     z_temp += z_table[8];
   }
   else {
-    printf("8: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 8);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 8);
     z_temp -= z_table[8];
@@ -145,13 +126,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("9: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 9);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 9);
     z_temp += z_table[9];
   }
   else {
-    printf("9: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 9);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 9);
     z_temp -= z_table[9];
@@ -160,13 +139,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("10: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 10);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 10);
     z_temp += z_table[10];
   }
   else {
-    printf("10: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 10);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 10);
     z_temp -= z_table[10];
@@ -175,13 +152,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("11: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 11);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 11);
     z_temp += z_table[11];
   }
   else {
-    printf("11: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 11);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 11);
     z_temp -= z_table[11];
@@ -190,13 +165,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("12: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 12);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 12);
     z_temp += z_table[12];
   }
   else {
-    printf("12: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 12);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 12);
     z_temp -= z_table[12];
@@ -205,13 +178,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("13: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 13);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 13);
     z_temp += z_table[13];
   }
   else {
-    printf("13: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 13);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 13);
     z_temp -= z_table[13];
@@ -220,13 +191,11 @@ void cordic_V_fixed_point( int *x, int *y, int *z) {
   y_temp_1 = y_temp_2;
 
   if( y_temp_1 > 0) {
-    printf("14: FIRST spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 + (y_temp_1 >> 14);
     y_temp_2 = y_temp_1 - (x_temp_1 >> 14);
     z_temp += z_table[14];
   }
   else {
-    printf("14: SECOND spot, y_temp_1 = %d \n",y_temp_1);
     x_temp_2 = x_temp_1 - (y_temp_1 >> 14);
     y_temp_2 = y_temp_1 + (x_temp_1 >> 14);
     z_temp -= z_table[14];
